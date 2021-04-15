15b5be5838e25912351713d091f32d29868b47ec
nabinkim0318 <32893645+nabinkim0318@users.noreply.github.com>
2021-04-14 21:47:50 -0700

    Update mp3_feature_extractor.m

--- mp3_feature_extractor.m
+++ mp3_feature_extractor.m
@@ -1,7 +1,13 @@
-function [rough, key, clairty, energy] = mp3_feature_extractor(mp3)
+function [result] = mp3_feature_extractor(mp3) 
     rough = mean(mirgetdata(mirroughness(mp3)))
-    keyname(kkkey(mp3))
     clarity = mirpulseclarity(mp3)
     energy = mirlowenergy(mp3) 
+    tempo = gettempo('Q1__8v0MFBZoco_1.mp3')
+    avg_silence = mirlowenergy('Q1__8v0MFBZoco_1.mp3', 'ASR')
+    key = keyname(kkkey(mp3))
+    tonality = keymode('Q1__8v0MFBZoco_1.mp3') - 1 
     
+    result = [rough, clarity, energy, tempo, avg_silence, key, tonality]
 end
+
+
